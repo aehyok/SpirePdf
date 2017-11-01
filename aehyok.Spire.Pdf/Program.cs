24ee5e75f5ecb56732e1aefe367c05a278d8bf89
刘启明 <aehyok@vip.qq.com>
2017-11-01 18:25:25 +0800

    生成一段PDF，分成四行 指定每行的字数进行循环遍历

--- aehyok.Spire.Pdf/Program.cs
+++ aehyok.Spire.Pdf/Program.cs
@@ -11,6 +11,8 @@ using System.Threading.Tasks;
 
 namespace aehyok.Spire.Pdf
 {
+    //文档
+    //https://www.e-iceblue.cn/spirepdf/create-a-new-pdf-document.html
     class Program
     {
         static void Main(string[] args)
@@ -19,7 +21,12 @@ namespace aehyok.Spire.Pdf
             var key = "JcDzkPGvDstH51n5AQCVLchN6Syd / f / T2NIQMipJsF6sB1LLhPz64WKU0qn0uCjkUr8qMEbUOHHPAK0UzNlLC2Ri + UQt1 + 4kEbc5Mrelr6 / d2X6af91nFCC9IP9P5a0kmFMMfFMw5dE9luSw5YRTt7EgQvrAGOQ0cIsCO5VGyPqH / VV5X7DPrqda2iAKLRBnPOGRuq2Lvs9TgRJ + ZROFwkckqX8NXhPDXaFST01Cpy6azavDyiASnkiGCsYy6poTnkRK / AlOHkZMOG0EDUQkAmxa00f + F03qzRPLWS7djki4gL8 / JmMBGALc + v6NlSuobR8XXj + OpJRkQiteP8XxsVe / 9sdpqmXnJUmv1XKNVggLyeIKOt4temmZc5yWIjkwTZp6Z66p8 / WE8ahkRA2HdGOAitOzOhCvBch + X + gEXlmnCcteryqe / tYa + QNoVFMvSuW6h + 2 + a4owhuSj0x3vO + BgY3YpoelZkc3mJl6DnGNdd6 / r4HE9N1fTGPT2gXY3Li1 / G / NwB +/ EUDCb8DUrTgysCVs3FM + Gnufbql0o3cswikfjF26Rdp3QtosHosmCAEUxiqBv8N8AwyCKTVT4rYUn3oRQZTBAaXtr / uZd2mNC5fZqslWqJFml5V1cNSf6K8x8F3wcO2lfytsWKzB4BkUPRbJZG / dAyUccrFKlLceVktmF0bajPIp5aN02NZfuXD9wfyf9g2ic / 0l3VACaWS7QFVNf7ZDxmmfssagM0fjpk0eBY3eFt8 / I8DhIo3739JkxDcgfNQ0nd7jrvX1iGCQUwaCyUfZllphsmZN4SkbWIoQX / UXW3swyXnVDKhp / Cl27RvXVHP5n3iQgya8Tun0dvnadCAn / awKvyOu5OyJ / j77hCFWFHpWPpGsZxQMYtODrTnHkz2 / 6rQNc9lBEnBkipieEMvEiSyk + jENeLI0IlmZpCyCrjTpJPHfRZJkZbIAaYRhhYoyl5o / xmldJaTe1UdBCz3N0Z0dpzY2amdcA + sOUjkogdD2zbwUiLwkzf7rQDQSYEUI / 0uVgz7PIQGuCOlqAtdUwCpRs + ktwkHWr0SoAgZAlu3N9YVIB + rQlPfudP2wGUYpUfT3ZwRezrBSjLUCs3ou2mFrhKkfwaATf90gAnQLhJQ6InPREzqjGj1ZB + 3q5ymjA0svrjM2Fw3EFALufHGBEChdHxL / ztiy7NcVX5f5V4XAoUybbhWMVcKUuZJgRIFK8JfawPPI + Z7jz94pPxDAeCBVg + 7oBKWGjkLB0 + RIYPIOKAMiuo7pCMNASd5etkSPmo7L / J1pxnCxado6emKBgcDqqAN + PpnTwu9pmATjrPGL3plP4BnNLmT36jlNPWVMd4NvvksGM4N49EwCzlbGx57FObBDSH5VJYszapDc217jpuGHKz4XUP6m5jfxL2IZfdrRrQJxepg ==";
             LicenseProvider.SetLicenseKey(key);
             #endregion
+            //Create_01();
+            Create_02();
+        }
 
+        public static void Create_01()
+        {
             ///1、创建一个PDF 的Document
             PdfDocument doc = new PdfDocument();
             PdfUnitConvertor unitCvtr = new PdfUnitConvertor();
@@ -56,8 +63,9 @@ namespace aehyok.Spire.Pdf
             //PdfTrueTypeFont font1 = new PdfTrueTypeFont(new Font("Arial", 16f, FontStyle.Bold));
 
             var fontUrl = "C:/WINDOWS/Fonts/AdobeSongStd-Light.otf";
-            PdfTrueTypeFont font1=new PdfTrueTypeFont(new Font("Arial", 16f),true);
+            PdfTrueTypeFont font1 = new PdfTrueTypeFont(new Font("Arial", 16f), true);
             PdfStringFormat format1 = new PdfStringFormat(PdfTextAlignment.Center);
+            
             page.Canvas.DrawString("Country dsfa列表", font1, brush1, page.Canvas.ClientSize.Width / 2, y, format1);
             y = y + font1.MeasureString("Country列表", format1).Height;
             y = y + 5;
@@ -93,6 +101,65 @@ namespace aehyok.Spire.Pdf
             doc.SaveToFile("SimpleTable.pdf");
             doc.Close();
             ///9、打开刚刚制作的文件
+            System.Diagnostics.Process.Start("SimpleTable.pdf");
+        }
+
+
+        public static void Create_02()
+        {
+            //创建一个PDF文档
+            PdfDocument doc = new PdfDocument();
+
+            //添加新页
+            PdfPageBase page = doc.Pages.Add();
+
+
+            var fontUrl = @"C:/WINDOWS/Fonts/AdobeSongStd-Light.otf";
+            //实例化一个TrueType font对象
+            PdfTrueTypeFont trueTypeFont = new PdfTrueTypeFont(fontUrl, 12);
+
+
+            PdfStringFormat pdfStringFormat = new PdfStringFormat();
+            pdfStringFormat.ParagraphIndent = 50;
+
+            var SubText = @"中国根据《中华人民共和国海关法》第九十三条、《中华人民共和国海关行政处罚,施条例》第六十条的规定，"
+                                        + "当事人逾期不履行处罚决定又不申请复议或者向人民法院提起诉讼的，海关可以将扣留的货物、物品、"
+                                        +"运输工具依法变价抵缴，"
+                                        + "或者以当事人提供的担保抵缴；也可以申请人民法院强制执行。";
+
+            float y = 10;
+            var index = 0;
+            PdfStringFormat format2 = new PdfStringFormat(PdfTextAlignment.Justify);
+            for (var i = 0; i < 4; i++)
+            {
+                if (index == 0)
+                {
+                    page.Canvas.DrawString(SubText.Substring(0, 36), trueTypeFont, PdfBrushes.Black, 36, y, format2);
+                }
+                else
+                {
+                    if(i<3)
+                    {
+                        page.Canvas.DrawString(SubText.Substring(index, 38), trueTypeFont, PdfBrushes.Black, 10, y, format2);
+                    }
+                    else
+                    {
+                        page.Canvas.DrawString(SubText.Substring(index, SubText.Length-index), trueTypeFont, PdfBrushes.Black, 10, y, format2);
+                    }
+                    
+                }
+
+                y = y + trueTypeFont.Height + 1 + i;
+
+                index = index + 38;
+            }
+
+
+            //page.Canvas.DrawString("999999999999999999999999999",
+                                       // trueTypeFont, PdfBrushes.Black, 30,50, pdfStringFormat);
+
+            //保存文档
+            doc.SaveToFile("SimpleTable.pdf");
             System.Diagnostics.Process.Start("SimpleTable.pdf");
         }
     }
